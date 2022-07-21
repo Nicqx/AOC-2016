cbf2e08a1bd40b0d9eca8c5d57450a77a19ba0db
Nicqx <miklos.d.parrag@gmail.com>
2022-07-21 17:54:20 +0200

    eighth day resolved

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,13 @@ import Days.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello 2016 Advent!!!");
-        new Day1();
-        new Day2();
-        new Day3();
-        new Day4();
-        new Day5();
-        new Day6();
-        new Day7();
+//        new Day1();
+//        new Day2();
+//        new Day3();
+//        new Day4();
+//        new Day5();
+//        new Day6();
+//        new Day7();
+        new Day8();
     }
 }
