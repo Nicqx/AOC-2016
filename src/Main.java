8bb454187d7c8b71d8e1e60b7126116166bc43bb
Nicqx <miklos.d.parrag@gmail.com>
2022-08-23 12:46:38 +0200

    fourth day test

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello 2016 Advent!!!");
 //        new Day1();
-        new Day2();
+//        new Day2();
 //        new Day3();
-//        new Day4();
+        new Day4();
 //        new Day5();
 //        new Day6();
 //        new Day7();
